910f33077e7e71809f1ac9f42851e1c98adc9328
Andrew Madsen <andrew@openreelsoftware.com>
2013-01-07 15:35:58 -0700

    Initial Commit

---
+++ DKBLE112 Demo/ORSViewController.m
@@ -0,0 +1,29 @@
+//
+//  ORSViewController.m
+//  DKBLE112 Demo
+//
+//  Created by Andrew Madsen on 1/7/13.
+//  Copyright (c) 2013 Open Reel Software. All rights reserved.
+//
+
+#import "ORSViewController.h"
+
+@interface ORSViewController ()
+
+@end
+
+@implementation ORSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ DKBLE112 Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DKBLE112 Demo
+//
+//  Created by Andrew Madsen on 1/7/13.
+//  Copyright (c) 2013 Open Reel Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ORSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ORSAppDelegate class]));
+	}
+}
